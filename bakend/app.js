450e683a33cd3b4011319f8c51d58f5f535c72fb
peyagodson <mossaka2018>
2019-04-01 21:42:26 +0300

    app done

---
+++ bakend/app.js
@@ -0,0 +1,39 @@
+//MongoDB connection
+//mongodb+srv://root:root@cluster0-fbvwl.mongodb.net/test?retryWrites=true
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
+const path = require('path');
+
+const app = express();
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
+mongoose.connect('mongodb+srv://root:root@cluster0-fbvwl.mongodb.net/test?retryWrites=true',{ useNewUrlParser: true })
+    .then(() => {
+        console.log('Successfully connected to MongoDB Atlas!');
+    })
+    .catch((error) => {
+        console.log('Unable to connect to MongoDB Atlas!');
+        console.error(error);
+    });
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+
+app.use(bodyParser.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
